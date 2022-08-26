f981811de79029cab9ead8fe8d49ea66a8f80894
Eduardo Queiros <code36u4r60@hotmmail.com>
2022-08-26 02:43:26 +0100

    update colores and add animation

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
-const btn = document.querySelector('#back-to-top')
-const allFamily = document.querySelectorAll('.all-family')
-const m18 = document.querySelectorAll('.m18')
+const btn = document.querySelector("#back-to-top");
+const allFamily = document.querySelectorAll(".all-family");
+const m18 = document.querySelectorAll(".m18");
 
 function scrollFunction() {
-  if (
-    (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
-  ) {
-    btn.style.display = 'block'
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btn.style.display = "block";
   } else {
-    btn.style.display = 'none'
+    btn.style.display = "none";
   }
 }
 
 function topFunction() {
-  document.body.scrollTop = 0
-  document.documentElement.scrollTop = 0
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
 function toggleTheVisibleItems() {
-  m18.forEach(element => {
-    element.classList.toggle('hidden')
-  })
-  allFamily.forEach(element => {
-    element.classList.toggle('hidden')
-  })
+  m18.forEach((element) => {
+    element.classList.toggle("hidden");
+  });
+  allFamily.forEach((element) => {
+    element.classList.toggle("hidden");
+  });
 }
 
-window.onscroll = () => scrollFunction()
+window.onscroll = () => scrollFunction();
